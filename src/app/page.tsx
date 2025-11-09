d1eec4dd201a60d6da0f45a4243d47bee91e7338
Kunal Titare <croma@cromas-MacBook-Air.local>
2025-11-10 03:15:34 +0530

    clerk users sync to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ import HowItWorks from "@/components/landing/HowItWorks";
 import PricingSection from "@/components/landing/PricingSection";
 import CTA from "@/components/landing/CTA";
 import Footer from "@/components/landing/Footer";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  if(user) redirect("/dashboard")
   return (
     <div className="min-h-screen bg-background">
       < Header />
